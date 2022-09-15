29c543e9ff178aa756e0a983102a13292ee62bf0
Artem Tabachenko <fexo5000@icloud.com>
2022-09-15 17:50:28 +0300

    Implemented a search for things and more

--- FirstHomework.playground/Contents.swift
+++ FirstHomework.playground/Contents.swift
@@ -1,12 +1,12 @@
 import UIKit
 import Foundation
 
-class Creature {
+class Creature: founding {
+    
     var name: String
     var hp: Double
     var mp: Double
     var damage: Double
-    var defenceMultiplyer = Double.random(in: 0.5..<1)
     var isDied = false
     
     init(name: String, hp: Double, mp: Double, damage: Double) {
@@ -16,20 +16,41 @@ class Creature {
         self.damage = damage
     }
     
-    func attack(to enemy: Creature) {
-        if isDied == false {
-            enemy.hp -= damage * defenceMultiplyer
-            print("\(self.name) attaked \(enemy.name). His HP now: \(enemy.hp).")
-        } else {
-            print("\(enemy.name) already died.")
+    var defenceMultiplyer = Double.random(in: 0.5..<1)
+    func attack(to enemy: Creature) {}
+    
+    func foundingItem(item: foundItem) {
+        if item == .rareItem {
+            self.hp += 3
+            self.mp += 1
+            print("\(self.name) found rare item! Now his HP: \(self.hp) and MP: \(self.mp).")
+        }
+        if item == .uncommonItem {
+            self.hp += 2
+            self.mp += 0.5
+            print("\(self.name) found uncommon item! Now his HP: \(self.hp) and MP: \(self.mp).")
+        }
+        if item == .commonItem {
+            self.hp += 1
+            print("\(self.name) found common item! Now his HP: \(self.hp).")
         }
     }
+}
+
+enum foundItem {
+    case rareItem
+    case uncommonItem
+    case commonItem
+}
+
+protocol founding {
+    func foundingItem(item: foundItem)
 }
 
 
 class Elf: Creature {
     override func attack(to enemy: Creature) {
-        var mpMultiplyer = Double.random(in: 0.2..<1)
+        var mpMultiplyer = Double.random(in: 0.5..<1)
         if isDied == false {
             enemy.hp -= (damage + (mp*mpMultiplyer)) * defenceMultiplyer
             print("\(self.name) attaked \(enemy.name). His HP now: \(enemy.hp).")
@@ -55,15 +76,20 @@ class Human: Creature {
     }
 }
 
+class Monster: Creature {
+    override func attack(to enemy: Creature) {
+        if isDied == false {
+            enemy.hp -= (damage + mp/2) * defenceMultiplyer
+            print("\(self.name) attaked \(enemy.name). His HP now: \(enemy.hp).")
+        } else {
+            print("\(enemy.name) already died.")
+        }
+    }
+}
 
-
-class Monster: Creature {}
-
-var creature = Creature(name: "Alex", hp: 10, mp: 3, damage: 6)
 var elf = Elf(name: "Max", hp: 13, mp: 5, damage: 2)
 var human = Human(name: "Ivan", hp: 14, mp: 2, damage: 5)
+var monster = Monster(name: "Giena", hp: 12, mp: 2, damage: 3)
 
-//creature.attack(to: elf)
-
-elf.attack(to: creature)
 human.attack(to: elf)
+elf.foundingItem(item: .rareItem)
