604923af4648699a3f46998e618282d7dab0340b
Ferogama <megakronger52@icloud.com>
2022-09-30 00:07:34 +0300

    let start

---
+++ homeworks/hw3/hw3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw3
+//
+//  Created by Meiers on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
