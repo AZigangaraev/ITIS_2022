f28aa62fab3e9a4f14fe7841857e5f300b1d6b39
goku <khayrullin-timurka@mail.ru>
2022-10-08 23:36:06 +0300

    Add files via upload

---
+++ HW4/HW4/HelloViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HelloViewController.swift
+//  HW4
+//
+//  Created by Тимур Хайруллин on 07.10.2022.
+//
+
+import UIKit
+
+class HelloViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
