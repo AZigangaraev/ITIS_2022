760cf20d3657f4ec5b6f6065ae54af32c36260d0
uevuu <nikita18.marin@gmail.com>
2022-10-15 00:38:49 +0400

    done hw5

--- 105/hw5/hw5/SceneDelegate.swift
+++ 105/hw5/hw5/SceneDelegate.swift
@@ -4,12 +4,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: scene)
-        window.rootViewController = ViewController()
-        self.window = window
-        window.makeKeyAndVisible()
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
