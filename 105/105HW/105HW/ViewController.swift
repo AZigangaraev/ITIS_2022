b762d2d3b6ed614c5c9a78de5cecb98d1905e9e1
Karina <karihairullina@gmail.com>
2022-10-15 15:33:01 +0300

    rm file

---
+++ 105/105HW/105HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  105HW
+//
+//  Created by Карина Хайрулина on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
